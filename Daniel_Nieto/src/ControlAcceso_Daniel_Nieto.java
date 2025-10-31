5f221a93bab21faef7bf2014370a38e3cbe7ad09
A3Alumno13 <daniel07nieto@gmail.com>
2025-10-31 13:05:17 +0100

    Ejercicio Ampliación

---
+++ Daniel_Nieto/src/ControlAcceso_Daniel_Nieto.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class ControlAcceso_Daniel_Nieto {
+
+    //No me acordaba de como usar el MyScanner
+    private static final MyScanner sc = new MyScanner();
+
+    public static void main(String[] args) {
+        boolean salida;
+        int intentos = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce la edad: ");
+        int edad = sc.nextInt();
+            if  (edad > 120 || edad <= 0) {
+                System.out.println("Error, ¿seguro que estas vivo?");
+                salida = false;
+                intentos++;
+                System.out.println("Tienes : " + intentos + " intento " + " de 3");
+            } else {
+                salida = true;
+            }
+        // Bucle
+        while (intentos < 3 && !salida); {
+            if (edad < 18){
+                System.out.println("Acceso denegado: menor de edad.");
+
+            } else if (edad >= 18 && edad <= 65) {
+                System.out.println("Acceso permitido.");
+
+            }
+            else if (edad > 65) {
+                System.out.println("Acceso con beneficios para jubilados.");
+
+            }
+            else  {
+                System.out.println("Acceso incompleto.");
+            }
+        }
+        sc.close();
+    }
+}
